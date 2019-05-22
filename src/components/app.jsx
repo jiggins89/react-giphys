a4856a4344f9d431cb3234f4e3a2a86e3263cabd
John Higgins <johnhiggins@outlook.ie>
2019-05-22 17:08:28 +0100

    created app layout and search component

---
+++ src/components/app.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import SearchBar from './search.jsx';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="left-scene">
+          <SearchBar />
+          <div className="selected-gif"></div>
+        </div>
+        <div className="right-scene">
+          <div className="gif-list"></div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/search.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <input className="form-search" type="text" />
+    );
+  }
+}
+
+export default SearchBar;

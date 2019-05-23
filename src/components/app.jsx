78ed2791f0c4438a0d2c8b9ca02d518f34a04058
John Higgins <johnhiggins@outlook.ie>
2019-05-23 17:04:23 +0100

    added features

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,17 +1,43 @@
 import React, { Component } from 'react';
+import giphy from 'giphy-api';
 
 import SearchBar from './search.jsx';
+import Gif from './gif.jsx';
+import GifList from './gif_list.jsx';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      id: "YSN3blb8lUyJhc0qEg",
+      gifs: []
+    };
+
+    this.search('homer');
+  }
+
+  search = (query) => {
+    giphy('6GksLxPxWZbCpsAKsQbethgZ627OipB9').search({
+      q: query,
+      rating: 'g'
+    }, function (error, result) {
+      console.log(result.data);
+    });
+  }
+
   render() {
     return (
       <div>
         <div className="left-scene">
-          <SearchBar />
-          <div className="selected-gif"></div>
+          <SearchBar searchFn={this.props.search} />
+          <div className="selected-gif">
+            <Gif id={this.state.id} />
+          </div>
         </div>
         <div className="right-scene">
-          <div className="gif-list"></div>
+          <GifList gifs={this.state.gifs} />
         </div>
       </div>
     );
--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  handleUpdate = (e) => {
+    this.props.search = e.target.value;
+  }
+
   render() {
     return (
-      <input className="form-search" type="text" />
+      <input className="form=control form-search" type="text" onChange={this.handleUpdate} />
     );
   }
 }
